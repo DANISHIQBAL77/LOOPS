512352feddc548a11dc68df30618e3505196fac3
Danish Iqbal <di41141455@gmail.com>
2025-03-11 14:36:01 +0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,30 @@
+
+
+
+//LOOPS
+      //To execute a piece of code again and again
+
+      for(let i = 1; i <= 100; i++){
+        console.log("coding");
+      }
+
+      //Calculate sum of 1 to n
+    
+      let sum = 0;
+      let n =10;
+      for (let i = 1; i <= n; i++) {
+        sum = sum + i;
+      }
+      console.log("sum =" , sum);
+
+
+
+
+      //while LOOP
+      
+      let i = 1;
+      while (i <= 10){
+        console.log("coding");
+        i++;
+      }
+      
