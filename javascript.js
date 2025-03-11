4028dab1d1f6e962fedc4eb17aba322dceacc1b7
Danish Iqbal <di41141455@gmail.com>
2025-03-11 15:36:31 +0500

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -4,6 +4,8 @@
 //LOOPS
       //To execute a piece of code again and again
 
+
+      //for loop
       for(let i = 1; i <= 100; i++){
         console.log("coding");
       }
@@ -20,7 +22,7 @@
 
 
 
-      //while LOOP
+      //while loop
       
       let i = 1;
       while (i <= 10){
@@ -28,3 +30,37 @@
         i++;
       }
       
+      //do while loop
+
+
+
+      let j = 1;
+      do {
+        console.log("coding");
+        j++;
+      } while (j <= 10);
+      
+      //for in loop (for objects)
+      
+      let person = {name: "John", age: 30, city: "New York"};
+      for (let key in person) {
+        console.log(key + " : " + person[key]);
+      }
+      
+      //for of loop (for arrays)
+      
+      let colors = ["red", "blue", "green"];
+      for (let color of colors) {
+        console.log(color);
+      }
+          
+      //break statement
+      
+      for (let i = 0; i < 5; i++) {
+        if (i === 3) {
+          break;
+        }
+        console.log(i);
+      }
+      
+      //continue statement
